9a18aee427c0954183482f5dd53b1e7d193cf63b
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2021-11-22 18:35:55 +0100

    Navbar accessibility OK

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,31 @@
-// HAMBURGER MENU ----------------------------------------------
-
-// Open the menu ---------------------------------------------
+// HAMBURGER MENU 🍔 -------------------------------------------------------------------------------
 const hamburger = document.querySelector(".navbar__hamburger");
 const navMenu = document.querySelector(".navbar__menu");
+const navLink = document.querySelectorAll(
+  ".navbar__menu__link, .navbar__menu__contact"
+);
 
-hamburger.addEventListener("click", mobileMenu); 
+// Open the menu ---------------------------------------------
+hamburger.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+
+  if (navMenu.classList.contains("active")) {
+    // Toggle accesibility status
+    hamburger.setAttribute("aria-expanded", true);
+  } else {
+    hamburger.setAttribute("aria-expanded", false);
+  }
 }
 
 // Close the menu -------------------------------------------
-const navLink = document.querySelectorAll(".navbar__menu__link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+
+  hamburger.setAttribute("aria-expanded", false); // Toggle accesibility status
 }
