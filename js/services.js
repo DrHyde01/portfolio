3d8828a50610ea3ac2875837c2f74950670163f2
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-01-06 15:35:43 +0100

    🚧 Working on services page

---
+++ js/services.js
@@ -0,0 +1,22 @@
+// RANDOM ICON GENERATOR -----------------------------------------------------------------------
+const randomIcon = [
+  "intro__icon fas fa-palette servicesicon",
+  "intro__icon fas fa-code servicesicon",
+  "intro__icon fas fa-tools servicesicon",
+  "intro__icon fas fa-eye servicesicon",
+  "intro__icon far fa-lightbulb servicesicon",
+];
+
+const icon = document.querySelector(".intro__icon");
+
+function getRandomIcon() {
+  setInterval(() => {
+    // To change word every 5s
+    const indx = Math.floor(Math.random() * randomIcon.length); // Pick a random number of the array length
+    const iconNew = randomIcon[indx]; // Pick the associate icon link in array
+
+    icon.className = iconNew; // Put the icon link in the html
+  }, 5000);
+}
+
+getRandomIcon();
